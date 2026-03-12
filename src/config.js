1d0d75cfeeb579f41c8a857a3566b15fd5051506
Allen Abraham <allenabraham106@gmail.com>
2026-03-12 00:12:35 -0400

    Just wanted twitter. I will get X later

--- src/config.js
+++ src/config.js
@@ -12,5 +12,5 @@ export const site = {
 export const social = [
   { href: "https://www.linkedin.com/in/allen-abraham-4711082a4/", label: "LinkedIn", icon: "LinkedIn" },
   { href: "https://github.com/allenabraham106", label: "GitHub", icon: "GitHub" },
-  { href: "https://www.instagram.com/allenk.abraham/", label: "Instagram", icon: "Instagram" },
+  { href: "https://x.com/allenk_Abraham", label: "X", icon: "X" },
 ];
